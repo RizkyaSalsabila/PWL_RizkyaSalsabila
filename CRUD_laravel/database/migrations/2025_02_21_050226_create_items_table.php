b31f4fe8943a190728829b8c32059b1dab008711
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-02-21 12:52:24 +0700

    add migration 'items' table

--- CRUD_laravel/database/migrations/2025_02_21_050226_create_items_table.php
+++ CRUD_laravel/database/migrations/2025_02_21_050226_create_items_table.php
@@ -9,15 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up(): void      //membuat tabel tanpa mengembalikan nilai
     {
-        Schema::create('items', function (Blueprint $table) {
-            $table->id();               //kolom 'id' sebagai primary key dengan tipe BIGINT dan auto-increment.             
-            $table->string('name');     //kolom 'name' untuk menyimpan nama item dengan tipe VARCHAR(255).
-            $table->text('description');//kolom 'description' untuk menyimpan deskripsi barang dengan tipe TEXT.
-            $table->integer('stock');   //kolom 'stock' untuk menyimpan jumlah stok barang dalam bentuk bilangan bulat(INTEGER).
-            $table->integer('price');   //kolom 'price' untuk menyimpan harga barang dalam bentuk bilangan bulat(INTEGER).
-            $table->timestamps();       //untuk menambahkan kolom 'created_at' dan 'updated_at' secara otomatis.
+        Schema::create('items', function (Blueprint $table) {       //membuat tabel baru bernama 'items' pada database
+            $table->id();               //kolom 'id' sebagai primary key dengan tipe BIGINT dan auto-increment          
+            $table->string('name');     //kolom 'name' untuk menyimpan nama item dengan tipe VARCHAR(255)
+            $table->text('description');//kolom 'description' untuk menyimpan deskripsi barang dengan tipe TEXT
+            $table->integer('stock');   //kolom 'stock' untuk menyimpan jumlah stok barang dalam bentuk bilangan bulat(INTEGER)
+            $table->integer('price');   //kolom 'price' untuk menyimpan harga barang dalam bentuk bilangan bulat(INTEGER)
+            $table->timestamps();       //untuk menambahkan kolom 'created_at' dan 'updated_at' secara otomatis
         });
     }
 

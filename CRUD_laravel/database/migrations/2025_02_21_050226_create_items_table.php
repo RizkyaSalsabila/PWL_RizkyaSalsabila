84c379384ef38297be543460005af444bfefd5e8
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-02-21 12:30:56 +0700

    install laravel CRUD

---
+++ CRUD_laravel/database/migrations/2025_02_21_050226_create_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();               //kolom 'id' sebagai primary key dengan tipe BIGINT dan auto-increment.             
+            $table->string('name');     //kolom 'name' untuk menyimpan nama item dengan tipe VARCHAR(255).
+            $table->text('description');//kolom 'description' untuk menyimpan deskripsi barang dengan tipe TEXT.
+            $table->integer('stock');   //kolom 'stock' untuk menyimpan jumlah stok barang dalam bentuk bilangan bulat(INTEGER).
+            $table->integer('price');   //kolom 'price' untuk menyimpan harga barang dalam bentuk bilangan bulat(INTEGER).
+            $table->timestamps();       //untuk menambahkan kolom 'created_at' dan 'updated_at' secara otomatis.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};

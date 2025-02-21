5715e03685feefe3dec2c676941b85df035f2895
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-02-21 16:03:01 +0700

    add code in 'Item'

--- CRUD_laravel/app/Models/Item.php
+++ CRUD_laravel/app/Models/Item.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    //mengizinkan atribut berikut untuk mass assignment
+    protected $fillable = ['name', 'description', 'stock', 'price'];    //menambahkan kode ini karena laravel memiliki sistem keamanan untuk mencegah pengguna tidak sah memasukkan data yang tidak diizinkan
 }
